acab9591451d4ccc3fc5d03cd79f021e2aa9b686
nguyenxuandat20091985-rgb <2.5459457e+08+nguyenxuandat20091985-rgb@users.noreply.github.com>
2026-09-24 00:52:52 +0000

    chore: remove unused icon scripts

--- app-icons-init.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  if (!window.APP_ICONS) return;
-  var logo = document.getElementById('appLogo');
-  if (logo) logo.src = APP_ICONS.logo;
-  var fav = document.getElementById('appFavicon');
-  if (fav) fav.href = APP_ICONS.fav;
-  var apple = document.getElementById('appAppleIcon');
-  if (apple) apple.href = APP_ICONS.icon192;
-})();
