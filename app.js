c730656ffed259295a51b41067d8ae40cab82744
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-25 08:43:15 +0700

    fix: restore app with on-demand person form (base64 chunks)

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-PLACEHOLDER
+(function(){
+  var p=window.__A36||[];
+  if(!p.length){alert('Loi tai app');return;}
+  var bin=atob(p.join(''));
+  var bytes=new Uint8Array(bin.length);
+  for(var i=0;i<bin.length;i++) bytes[i]=bin.charCodeAt(i);
+  var code=new TextDecoder('utf-8').decode(bytes);
+  (0,eval)(code);
+})();
